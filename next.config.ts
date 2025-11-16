21fe82ac6fa35f4d01fd2620980dae28c7b2e578
vijayshreekrishna <myworkat2023@gmail.com>
2025-11-17 00:55:49 +0530

    imptemet of server actions and caching and event details page

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-    images:{
-        remotePatterns:[
+
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    cacheComponents: true,
+    images: {
+        remotePatterns: [
             {
-                protocol:'http',
-                hostname:'res.cloudinary.com',
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
             }
         ]
     },
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-    ];
-  },
-
-  // This is required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true,
+    async rewrites() {
+        return [
+            {
+                source: "/ingest/static/:path*",
+                destination: "https://us-assets.i.posthog.com/static/:path*",
+            },
+            {
+                source: "/ingest/:path*",
+                destination: "https://us.i.posthog.com/:path*",
+            },
+        ];
+    },
+    // This is required to support PostHog trailing slash API requests
+    skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
